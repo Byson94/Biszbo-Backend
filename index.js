d255c86d5e9c0c71275b65ac96d86d6144d489f8
Byson94 <byson94@outlook.com>
2025-04-30 21:46:40 +0530

    quick fix for new origin

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://byson94.github.io/Biszbo/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
