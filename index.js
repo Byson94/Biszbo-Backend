8e9a3286c25f59fc5c47df77469d36e327933855
Byson94 <byson94@outlook.com>
2025-04-30 22:49:03 +0530

    new item to allowed cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = ["http://localhost:5173", "https://byson94.github.io"];
+const allowedOrigins = ["http://localhost:5173", "https://byson94.github.io", "https://biszbo.onrender.com/"];
 
 app.use(
   cors({
